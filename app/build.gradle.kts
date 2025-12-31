55973fdf5684986d95dbb94aa852999207dca127
StayyLit <kralik209@gmail.com>
2025-12-31 03:38:19 -0600

    Align Java and Kotlin JVM target to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,23 @@ plugins {
 }
 
 android {
-  namespace = "com.example.dashero"
-  compileSdk = 35
+    compileSdk = 34
 
-  defaultConfig {
-    applicationId = "com.example.dashero"
-    minSdk = 26
-    targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
-  }
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
 
   buildFeatures { compose = true }
   composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
   id("org.jetbrains.kotlin.android") version "2.0.21" apply false
   id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
+
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
